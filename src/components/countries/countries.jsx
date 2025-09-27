3c85e8010660e1f3764ef5f9ea543e10ddbad698
Fahimpro-jpg <fahadalamfahim111844@gmail.com>
2025-09-27 19:07:11 +0600

    depolying in netlify

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,8 +1,47 @@
+import { use, useState } from "react";
+import Country from "../Country/Country";
+import './Countries.css'
 
-const Countries = () => {
+// this is api component
+const Countries = ({countriesPromise}) => {
+    const [visitedCountries,setVisitedCountries]=useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([]);
+    const handleVisitedCountries = (country)=>{
+        const newVisitedCountries = [...visitedCountries,country];
+        setVisitedCountries(newVisitedCountries)
+    }
+
+    const handleVisitedFlag = (flag)=>{
+       const newVisitedFlags = [...visitedFlags,flag];
+       setVisitedFlags(newVisitedFlags)
+    }
+    const countriesData = use(countriesPromise)
+    const countries = countriesData.countries
+    
     return (
         <div>
-            <h1>IN the countries</h1>
+            <h1>IN the countries: {countries.length}</h1>
+            <h3>Total Country Visited:     {visitedCountries.length}</h3>
+            <h3>Total Flags Visited: {visitedFlags.length}</h3>
+            <ol>
+                {
+                    visitedCountries.map(country=><li
+                    key={country.cca3.cca3}>{country.name.common}</li>)
+                }
+            </ol>
+            <div className="visited-flags-container">
+                {
+                    visitedFlags.map((flag,i)=><img key={i} src={flag}></img>)
+                }
+            </div>
+          <div className="countries">
+             {
+            countries.map(country=><Country country ={country}
+            key={country.cca3.cca3}
+            handleVisitedCountries={handleVisitedCountries}
+            handleVisitedFlag={handleVisitedFlag}></Country>)
+           }
+          </div>
         </div>
     );
 };
